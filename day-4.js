825597b1cc3d74a20309d73e39cf9268fcc9dafb
melihsah <melihsahtulek@gmail.com>
2022-03-30 01:29:43 +0300

    day-4 wip

--- day-4.js
+++ day-4.js
@@ -3,3 +3,19 @@
   DAY4:
 
 */
+
+console.log("this is a text. There is a number --> 0123".match(/a/gi));
+console.log("red color, black colooor".match(/o+/gi));
+console.log("color or colour".match(/colou?r/gi));
+console.log("this is a text. There is a number --> 0123".match(/./gi));
+console.log("this is a text. There is a number --> 0123".match(/n./gi));
+console.log("this is a text. There is a number --> 0123".match(/\w/gi));
+console.log("this is a text. There is a number --> 0123".match(/\W/gi));
+console.log("this is a text. There is a number --> 0123".match(/\s/gi));
+console.log("this is a text. There is a number --> 0123".match(/\S/gi));
+console.log("this is a text. There is a number --> 0123".match(/\d/gi));
+console.log("this is a text. There is a number --> 0123".match(/\d+/gi));
+console.log("this is a text. There is a number --> 0123".match(/\D/gi));
+console.log("this is a text. There is a number --> 0123".match(/\w{2}/gi));
+console.log("this is a text. There is a number --> 0123".match(/\w{2,5}/gi));
+console.log("this is a text. There is a number --> 0123".match(/\w{5,}/gi));
