efacaa2411cf4cf0b9c4e2375bd70a5758f355a5
melihsah <melihsahtulek@gmail.com>
2022-03-29 14:02:22 +0300

    day-2

---
+++ day-2.js
@@ -0,0 +1,10 @@
+/*
+
+  DAY 2:
+
+*/
+
+console.log(/^this/gi.test("this is a text."));
+console.log(/text.$/gi.test("this is a text."));
+console.log("version 1.0.0".match(/\d\.\d\.\d/gi));
+console.log("and , end".match(/[ae]nd/gi));
